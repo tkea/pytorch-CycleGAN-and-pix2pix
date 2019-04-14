9019da3255c666dc38a5e68c9e736ca2e060a64f
Tor Kristian Ertsvik Andreassen <tor.andreassen2@gmail.com>
2019-04-14 21:09:45 +0200

    test batch norm

--- models/pix2pix_improved_model.py
+++ models/pix2pix_improved_model.py
@@ -58,7 +58,7 @@ class Pix2PixImprovedModel(BaseModel):
 
         if self.isTrain:  # define a discriminator; conditional GANs need to take both input and output images; Therefore, #channels for D is input_nc + output_nc
             self.netD = networks.define_D(opt.input_nc + opt.output_nc, opt.ndf, opt.netD,
-                                          opt.n_layers_D, 'none', opt.init_type, opt.init_gain, self.gpu_ids)
+                                          opt.n_layers_D, 'batch', opt.init_type, opt.init_gain, self.gpu_ids)
 
         if self.isTrain:
             # define loss functions
